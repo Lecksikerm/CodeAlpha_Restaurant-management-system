99f2753d98e656908740ba533327b7c71de80bd7
kareem idris <idrisolalekann@gmail.com>
2025-12-05 20:40:34 +0100

    Create Menu item

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
+const menuRoutes = require("./routes/menu");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// Swagger setup
+const swaggerSpec = swaggerJsdoc({
+    definition: {
+        openapi: "3.0.0",
+        info: { title: "Restaurant API", version: "1.0.0" }
+    },
+    apis: ["./src/routes/*.js"]
+});
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/menu", menuRoutes);
+
+app.get("/", (req, res) => res.json({ status: "ok" }));
+
+module.exports = app;
---
+++ src/data-source.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const { DataSource } = require("typeorm");
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: true,
+    entities: [__dirname + "/entities/*.js"],
+    migrations: [__dirname + "/migrations/*.js"],
+});
+
+module.exports = { AppDataSource };
